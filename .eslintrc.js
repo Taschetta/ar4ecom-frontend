75b17ffb3c9187639afa9f7346843c68ad00b75f
santiago <santiagotaschetta@gmail.com>
2021-12-07 12:55:01 -0300

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,32 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/attributes-order': 'off',
+    'prefer-const': 'off',
+    'quote-props': 'off',
+    'no-undef': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 2,
+      },
+      multiline: {
+        max: 2,
+      },
+    }],
+  },
 }
