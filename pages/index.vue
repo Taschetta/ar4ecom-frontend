8eabfbaae5c5875b694cc6ea3e33cba7e8175a54
santiago <santiagotaschetta@gmail.com>
2021-12-07 12:56:36 -0300

    added / and /session/login pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <Tutorial />
+  <main>
+    <h2>Bienvenido a ar4ecom!</h2>
+  </main>
 </template>
 
 <script>
-export default {}
+import { useSesion } from '~/composition/index.js'
+export default {
+  setup () {
+    const $sesion = useSesion()
+
+    onMounted(async () => {
+      await $sesion.onlyAuthed()
+    })
+  },
+}
 </script>
---
+++ pages/sesion/iniciar.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <h2>Iniciar Sesión</h2>
+    <form id="FormLogin" @submit.prevent="login">
+      <label for="InputNombre">Nombre</label>
+      <input id="InputNombre" v-model="nombre" type="text">
+      <label for="InputContraseña">Contraseña</label>
+      <input id="InputContraseña" v-model="contraseña" type="password">
+    </form>
+    <nav>
+      <button form="FormLogin">
+        Iniciar Sesión
+      </button>
+    </nav>
+  </main>
+</template>
+
+<script>
+import { useSesion, useHandler } from '~/composition/index.js'
+
+export default {
+  setup () {
+    const { handle } = useHandler()
+
+    const $sesion = useSesion()
+    const $router = useRouter()
+
+    const nombre = ref('santi')
+    const contraseña = ref('ar4ecom')
+
+    const login = handle(async () => {
+      await $sesion.login({ nombre: nombre.value, contraseña: contraseña.value })
+      await $router.push('/')
+    })
+
+    return {
+      nombre,
+      contraseña,
+      login,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
