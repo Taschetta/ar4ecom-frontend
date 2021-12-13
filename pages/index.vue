53cf4fbc78ee8f375d5d863ef8cbda4edbc11c01
santiago <santiagotaschetta@gmail.com>
2021-12-13 17:33:46 -0300

    added /publicaciones and /suscripciones pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="stack stack-400">
-    <h2 class="card text-500 text-center">
+  <main class="card stack stack-500">
+    <h2 class="text-500 text-center">
       Bienvenido a ar4ecom!
     </h2>
-    <article class="card stack stack-200">
+    <article class="stack stack-200">
       <h3 class="text-400">
         Mis datos
       </h3>
@@ -13,21 +13,52 @@
         </nuxt-link>
       </nav>
     </article>
-    <button class="button button-error full-width">
+    <article class="stack stack-200">
+      <h3 class="text-400">
+        Publicaciones
+      </h3>
+      <nav>
+        <nuxt-link to="/publicaciones">
+          Listado
+        </nuxt-link>
+      </nav>
+    </article>
+    <article class="stack stack-200">
+      <h3 class="text-400">
+        Suscripciones
+      </h3>
+      <nav>
+        <nuxt-link to="/suscripciones">
+          Listado
+        </nuxt-link>
+      </nav>
+    </article>
+    <button class="button button-error full-width" @click="logout">
       Cerrar Sesion
     </button>
   </main>
 </template>
 
 <script>
-import { useSesion } from '~/composition/index.js'
+import { useSesion, useHandler } from '~/composition/index.js'
 export default {
   setup () {
     const $sesion = useSesion()
+    const $router = useRouter()
+    const $handle = useHandler()
+
+    const logout = $handle(async () => {
+      await $sesion.logout()
+      $router.push('')
+    })
 
     onMounted(async () => {
       await $sesion.onlyAuthed()
     })
+
+    return {
+      logout,
+    }
   },
 }
 </script>
---
+++ pages/suscripciones/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="card">
+    <h2 class="text-500">
+      Publicaciones
+    </h2>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
