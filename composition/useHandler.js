92df451c12055f6bb60c74d3b4cee05c6ede768c
santiago <santiagotaschetta@gmail.com>
2021-12-23 12:39:36 -0300

    handled 401 request error

--- composition/useHandler.js
+++ composition/useHandler.js
@@ -3,12 +3,18 @@ import { useNotification } from './useNotification.js'
 
 export const useHandler = () => {
   const $notification = useNotification()
+  const $router = useRouter()
 
   return callback => async (...args) => {
     try {
       return await callback(...args)
     } catch (error) {
-      $notification.insert({ message: error.message, type: 'error' })
+      if (error.name === 'RequestError' && error.status === 401) {
+        $notification.insert({ message: 'Se terminó su seción. Por favor, vuelva a loguearse', type: 'error' })
+        $router.push('/sesion/iniciar')
+      } else {
+        $notification.insert({ message: error.message, type: 'error' })
+      }
     }
   }
 }
